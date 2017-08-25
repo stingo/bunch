47f96b9894001139aacb39a5713771745053e5f6
Charles Wiredu <afrowebs@gmail.com>
2017-08-25 09:01:24 +0100

    song

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,23 +1,15 @@
 class Song < ApplicationRecord
-	belongs_to :profile, foreign_key:"profile_id"
+	belongs_to :profile, foreign_key: "profile_id"
 
 	mount_uploader :songcover, SongcoverUploader
-
 	mount_uploader :sound, SoundUploader
     
-    is_impressionable
+  is_impressionable
 
-
-
-	
-
-     extend FriendlyId
+  extend FriendlyId
   friendly_id :title, use: :slugged
 
    def should_generate_new_friendly_id?
     title_changed?
   end
-
-
-    
 end
