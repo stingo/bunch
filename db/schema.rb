8d6c840caad1805ab25c55e354c1f38d1f6aa3cf
Charles Wiredu <afrowebs@gmail.com>
2017-09-04 04:51:51 +0100

    companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831064717) do
+ActiveRecord::Schema.define(version: 20170902151643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
