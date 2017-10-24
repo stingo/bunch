eeffe5ea9a9d1e8fa3558a847fa343f85514da58
Charles Wiredu <afrowebs@gmail.com>
2017-10-24 19:43:37 +0100

    mib

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -5,7 +5,7 @@ class Industry < ApplicationRecord
 	has_many :jobs, through: :thing_industries
 	has_many :profiles, through: :thing_industries
 
-	belongs_to :profile, foreign_key:"profile_id"
+
 
 	 accepts_nested_attributes_for :thing_industries #allow_destroy: true
 end
