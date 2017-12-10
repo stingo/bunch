4e2dfe6af059115593b6064d8f716cee3dfd4101
Charles Wiredu <afrowebs@gmail.com>
2017-12-10 22:40:25 +0000

    stored location main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -50,8 +50,21 @@ end
      #@urban_terms = UrbanTerm.all
   #end
 
-  def set_redirect_path
-    @redirect_path = request.path
+  def store_location
+    if(request.path != "/profiles/sign_in" &&
+      request.path != "/profiles/sign_up" &&
+      request.path != "/profiles/password/new" &&
+      request.path != "/profiles/password/edit" &&
+      request.path != "/profiles/confirmation" &&
+      request.path != "/uprofiles/sign_out" &&
+      !request.xhr? && !current_profile) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+  def after_sign_in_path_for(resource)
+    previous_path = session[:previous_url]
+    session[:previous_url] = nil
+    previous_path || root_path
   end
 
 
