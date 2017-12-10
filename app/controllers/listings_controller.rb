c36b7e0c30e52a4b3f3b8ae340a29cda75992255
Charles Wiredu <afrowebs@gmail.com>
2017-12-10 23:12:50 +0000

    redirect to prev in all controllers

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -79,6 +79,26 @@ class ListingsController < ApplicationController
     end
   end
 
+
+  #Redirect to previous page after sign in
+
+   def store_location
+    if(request.path != "/profiles/sign_in" &&
+      request.path != "/profiles/sign_up" &&
+      request.path != "/profiles/password/new" &&
+      request.path != "/profiles/password/edit" &&
+      request.path != "/profiles/confirmation" &&
+      request.path != "/uprofiles/sign_out" &&
+      !request.xhr? && !current_profile) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+  def after_sign_in_path_for(resource)
+    previous_path = session[:previous_url]
+    session[:previous_url] = nil
+    previous_path || root_path
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
