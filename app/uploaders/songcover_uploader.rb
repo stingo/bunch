9bbee470ecc2f9626216ee5d63e2fd0172b1dfbc
Charles Wiredu <afrowebs@gmail.com>
2018-01-12 13:30:21 +0000

    mib

--- app/uploaders/songcover_uploader.rb
+++ app/uploaders/songcover_uploader.rb
@@ -35,7 +35,7 @@ class SongcoverUploader < CarrierWave::Uploader::Base
 
    # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fill => [300, 200]
+     process :resize_to_fill => [600, 400]
    end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
